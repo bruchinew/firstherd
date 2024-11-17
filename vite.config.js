fc6ebfdd73fccea917c7fc9b5e6934791dee524f
Bruchi Alter <bruchishe@gmail.com>
2024-11-17 18:28:33 +0200

    port binding

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     resolve: {
         alias: {
             ziggy: "node_modules/ziggy", // Ensure Ziggy is resolved correctly
         },
+    },
+    server: {
+        host: "0.0.0.0", // Bind to all interfaces
+        port: 5173, // You can change the port if needed
+        strictPort: true, // Ensures the server uses the specified port
     },
     plugins: [
         laravel({
