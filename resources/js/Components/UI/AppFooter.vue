efa51e6713f054b817f65baad1c625953a28b504
Bruchi Alter <bruchishe@gmail.com>
2024-12-23 13:29:17 +0200

    feat: appfooter

--- resources/js/Components/UI/AppFooter.vue
+++ resources/js/Components/UI/AppFooter.vue
@@ -1,8 +1,36 @@
 <template>
-    <div>
-        <h1>jijijijij</h1>
-        <h1>jijijijij</h1>
-        <h1>jijijijij</h1>
-        <h1>jijijijijv</h1>
+    <div class="footer-container">
+        <div class="content">
+            <button>
+                <a :href="route('quotes.index')">Contact Us</a>
+            </button>
+            <br />
+            <button>
+                <a :href="route('quotes.index')">Support</a>
+            </button>
+            <br />
+            <button>
+                <a :href="route('quotes.index')">Help</a>
+            </button>
+        </div>
     </div>
 </template>
+
+<script setup></script>
+
+<style scoped>
+.footer-container {
+    position: relative;
+    width: 100%;
+    height: 200px; /* Adjust the height as needed */
+    background: url("/images/wording.png") no-repeat center center;
+    background-size: contain, contain;
+    opacity: 0.1; /* Adjust the opacity for watermark effect */
+}
+
+.content {
+    position: relative;
+    z-index: 1;
+    /* Add any additional styling for your footer content */
+}
+</style>
